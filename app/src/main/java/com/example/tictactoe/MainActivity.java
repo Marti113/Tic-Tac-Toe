e0a1863633965b5e6d579446762fa9c83db21298
Marti Kandallu <marti.kandallu@gmail.com>
2020-07-22 19:18:23 -0700

    Initial commit

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.tictactoe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button[][] buttons = new Button[3][3];
+
+    private boolean player1Turn = true;
+
+    private int roundCount;
+
+    private int player1Points;
+    private int player2Points;
+
+    private TextView textViewPlayer1;
+    private TextView textViewPlayer2;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewPlayer1 = findViewById(R.id.text_view_p1);
+        textViewPlayer2 = findViewById(R.id.text_view_p2);
+
+        for (int i=0; i<3; i++){
+
+            for (int j=0; j<3;j++){
+                String buttonID = "button_" + i + j;
+                int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
+                buttons[i][j] = findViewById(resID);
+                buttons[i][j].setOnClickListener(this);
+            }
+        }
+        Button buttonReset = findViewById(R.id.button_reset);
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (!((Button) view).getText().toString().equals("")) {
+            return;
+        }
+        if (player1Turn){
+            ((Button) view).setText("X");
+        } else {
+            ((Button) view).setText("O");
+        }
+
+        roundCount++;
+
+    }
+
+    private boolean checkForWin() {
+        String[][] field = new String[3][3];
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                field[i][j] = buttons[i][j].getText().toString();
+            }
+        }
+        //checks rows for a winner
+        for (int i = 0; i < 3; i++) {
+            if (field[i][0].equals(field[i][1])
+                    && field[i][1].equals(field[i][2])
+                    && !field[i][0].equals("")) {
+                return true;
+            }
+        }
+        //checks columns for a winner
+        for (int i = 0; i < 3; i++) {
+            if (field[0][i].equals(field[1][i])
+                    && field[1][i].equals(field[2][i])
+                    && !field[0][i].equals("")) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
